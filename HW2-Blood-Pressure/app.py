84036d27ff0c20e1d72bc1d49f916d5aa11d740e
Manny <manny881@zoho.com>
2022-03-04 11:32:00 -0800

    updates

--- HW2-Blood-Pressure/app.py
+++ HW2-Blood-Pressure/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    # if request == "POST":
-    #     age = request.form["age"]
-    #     weight = request.form["weight"]
+    if request.method == "POST":
+        age = request.form["age"]
+        weight = request.form["weight"]
+        print(age, weight)
+        return redirect(url_for("index"))
 
-    #     print(age, weight)
     return render_template("index.html")
