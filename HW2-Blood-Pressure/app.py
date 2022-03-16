d8c3c176d1fbed4114a1a853092fba750dec8f74
Manny <manny881@zoho.com>
2022-03-15 20:42:34 -0700

    hw2 complete

--- HW2-Blood-Pressure/app.py
+++ HW2-Blood-Pressure/app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, render_template, request
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         age = request.form["age"]
         weight = request.form["weight"]
-        print(age, weight)
-        return redirect(url_for("index"))
+
+        pkl_clf = joblib.load("HW2-Blood-Pressure/regr.pkl")
+        df = pd.DataFrame([[age, weight]], columns=["Age", "Weight"])
+
+        pred = pkl_clf.predict(df)[0]
+        return render_template("index.html", data=pred)
 
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
